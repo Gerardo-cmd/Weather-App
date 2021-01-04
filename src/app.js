0f8ddf0356d0ec061c23bab0cdc1f160e126d5c8
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-01-04 13:56:48 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+
+const app = express()
+
+// Define paths for express configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Gerardo Abaunza'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Gerardo Abaunza'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Here are some clarifications if needed!',
+        title: 'Help',
+        name: 'Gerardo Abaunza'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        res.send({
+            error: 'Must provide an address!'
+        })
+    }
+    else {
+        geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return res.send({ error })
+                }
+
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+            })
+        })
+    }
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    
+    console.log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+// app.com
+// app.com/help
+// app.com/about
+// app.com/weather
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Help article not found',
+        name: 'Gerardo Abaunza'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found',
+        name: 'Gerardo Abaunza'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
